14bf57e8a470c1147d25cda6f2bff6b862b6e829
regenrek <hello@regenrek.at>
2021-01-25 21:51:49 +0100

    update lp

---
+++ components/bloks/BlokImageSlider.js
@@ -0,0 +1,13 @@
+import { forwardProps } from '@nujek/shared'
+import ImageSlider from '~/components/organisms/ImageSlider'
+
+export default {
+  name: 'BlokImageSlider',
+  functional: true,
+  props: ['blok'],
+  render(h, context) {
+    return h(ImageSlider, {
+      props: forwardProps(context.props.blok)
+    })
+  }
+}
--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>
-    <pre>
-        {{ $props }}
-    </pre>
-  </div>
+  <NjSection variant="boxed" class="py-16">
+    <div class="flex items-center">
+      <div class="px-8 lg:px-20">
+        <h1 class="text-2xl lg:text-5xl xl:text-6xl">
+          Awesome Products & innovative experiences
+        </h1>
+      </div>
+      <div class="image-stack">
+        <div class="image-stack__item image-stack__item--top">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/110238/portrait1.jpeg"
+            alt=""
+          />
+        </div>
+        <div class="image-stack__item image-stack__item--bottom">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/110238/texture-waves-cropped.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </NjSection>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.image-stack {
+  display: grid;
+  position: relative;
+  grid-template-columns: repeat(12, 1fr);
+}
+
+.image-stack__item--bottom {
+  grid-column: 4 / -1;
+  grid-row: 1;
+}
+
+.image-stack__item--top {
+  grid-row: 1;
+  grid-column: 1 / span 8;
+  padding-top: 20%;
+  z-index: 1;
+}
+
+img {
+  width: 100%;
+  display: block;
+}
+</style>
