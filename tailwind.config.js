7eec718f7295350fe3775a6c3729baed6650653d
Kevin Regenrek <hello@regenrek.at>
2021-02-17 21:45:41 +0100

    test deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   darkMode: 'class',
   purge: {
-    enabled: false,
-    options: {
-      safelist: [
-        '/aspect-ratio-/',
-        '/nj-video-background/',
-        '/swiper/',
-        '/has-aspect-ratio/'
-      ]
-    }
+    enabled: false
   },
   theme: {
     extend: {
@@ -17,14 +9,6 @@ module.exports = {
       colors: {
         primary: '#FFE152',
         'gray-lightest': '#efefef'
-      },
-      aspectRatio: {
-        '1/1': [1, 1],
-        '16/9': [16, 9],
-        '9/16': [9, 16],
-        '4/3': [4, 3],
-        '3/4': [3, 4],
-        '2/3': [2, 3]
       },
       minHeight: {
         520: '520px'
@@ -36,49 +20,7 @@ module.exports = {
         // Needed for NjSidebar
         400: 400,
         500: 500
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            h1: {
-              letterSpacing: '-1.2367px'
-            },
-            h2: {
-              letterSpacing: '-1.2367px'
-            },
-            h4: {
-              fontWeight: 500,
-              letterSpacing: '-1.2367px'
-            }
-          }
-        },
-        '3xl': {
-          css: {
-            fontSize: '1.875rem',
-            p: {
-              fontSize: '1.2rem'
-            },
-            a: {
-              fontSize: '1.2rem'
-            },
-            span: {
-              fontSize: '1.2rem'
-            },
-            li: {
-              fontSize: '1.2rem'
-            }
-            // ...
-          }
-        }
       }
     }
-  },
-  variants: {
-    aspectRatio: ['responsive']
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('./tailwind-plugins/aspect-ratio.js')()
-  ]
+  }
 }
