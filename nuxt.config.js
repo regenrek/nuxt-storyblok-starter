30c6d88ec8e212b8f9ea1ee44a3c85883f8dd92a
Kevin Regenrek <hello@regenrek.at>
2021-02-17 21:49:54 +0100

    test deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@nujek/ui/css/nujek-ui.css', '~assets/styles/main.css'],
+  css: ['~assets/styles/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
