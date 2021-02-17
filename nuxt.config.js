ec5e880b7e82befaced695a542d2842bcc215d83
Kevin Regenrek <hello@regenrek.at>
2021-02-17 22:02:35 +0100

    test deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,10 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@nujek/ui/css/components/video-background.css',
-    '~assets/styles/main.css'
-  ],
+  css: ['~assets/styles/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
